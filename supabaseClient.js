019eb51e7a5a2bf6deb4a62c5706046ef3ae83a3
jmarkbatallerwork-pixel <jmarkbataller.work@gmail.com>
2026-03-03 11:25:23 +0800

    Update supabaseClient.js

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from "https://esm.sh/@supabase/supabase-js@2"
 
-const SUPABASE_URL = "YOUR_SUPABASE_URL"
-const SUPABASE_ANON_KEY = "YOUR_SUPABASE_ANON_KEY"
+const SUPABASE_URL = "https://goekqwyjkkjsqvqftonf.supabase.co"
+const SUPABASE_ANON_KEY = "YeyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdvZWtxd3lqa2tqc3F2cWZ0b25mIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI0OTk3MTEsImV4cCI6MjA4ODA3NTcxMX0.9tdpPgM5e4yuSKB8DnBYsdd7RDJvXZ1ojFQkvvF1du8"
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
